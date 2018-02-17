b522192ee6325603e41e1ff50259b6cc99eb2e33
AlbertoPuig <alberto.puig.rguez@gmail.com>
2018-02-17 14:53:27 +0100

    change entry point and check param type

--- wsgi.py
+++ wsgi.py
@@ -6,36 +6,39 @@ import json
 
 application = Flask(__name__)
 
+#to check status of service
 @application.route("/<param>")
 def hello(param):
-    return "Hello Friend!...." + param
-
-@application.route("/edit/<param3>")
-def edit(param3):
+	print ("Request to / with param: " + param)
+	return "Service Enabled!...."
+    
+#api entry
+@application.route("/api/v1/quest/<qnumber>")
+def edit(qnumber):
 	result = ''
-	data_string = json.dumps(result)
-	#Decoded
-	decoded = json.loads(data_string)
-	question = 'Q'
-	question += param3
-	print ("--------------------------------------------")
-	print ("question " + question)
-	print ("--------------------------------------------")
-	try:
-		with open('data/data.json', 'r') as f:
-			data = json.load(f)
-			try:
-				question_data = data[question]
-				#logging.debug('Response for params: ' + question_id)
-				return make_response(jsonify(question_data),200)       
-			except Exception as ex:
-				print ("Not found. Param error")
-				return make_response(jsonify(),500)
-	except Exception as e:
-		#message to log
-		#logging.debug(str(e) + question_id)
-		#return error
-		return make_response(jsonify(e),500) 
+	if qnumber.isdigit():
+		data_string = json.dumps(result)
+		#Decoded
+		decoded = json.loads(data_string)
+		question = 'Q'
+		question += qnumber
+		print ("--------------------------------------------")
+		print ("question " + question)
+		print ("--------------------------------------------")
+		try:
+			with open('data/data.json', 'r') as f:
+				data = json.load(f)
+				try:
+					question_data = data[question]
+					print ("Responding json")
+					return make_response(jsonify(question_data),200)       
+				except Exception as ex:
+					print ("Not found. Param error")
+					return make_response(jsonify(),500)
+		except Exception as e:
+			return make_response(jsonify(e),500) 
+	else:
+		return abort(400)
 
 
 if __name__ == "__main__":
